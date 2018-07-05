abd63022901e6841ee26efa203b3e89ccb731f0d
dzhitser <admin@sofacms.ru>
2018-07-05 22:24:16 +0700

    relize index

--- index.php
+++ index.php
@@ -2,22 +2,8 @@
 <!DOCTYPE HTML>
 <html lang="ru">
 <head>
-    <meta charset="utf-8">
-    <meta name="robots" content="index, follow">
-    <meta name="author" content="htmlhook.ru">
-    <meta name="publisher" content="Viktor Klim">
-    <meta name="description" content="������� ������������� ������ HTML5">
-    <meta name="keywords" content="100%, ������, HTML5">
-    <meta name="generator" content="Core CMS">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-
-<title>100% ������ HTML5</title>
-
- <!--������ ��� ������ ��������� IE-->
-
- <!--[if lt IE 9]>
-    
- <![endif]-->
+  
+<title></title>
 
 </head>
 <body>
